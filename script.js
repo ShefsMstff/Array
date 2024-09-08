99a2bdb3c3b5b7a6349c5e5f029e76da293a0843
ShefsMstff <sheffmustafaeff@gmail.com>
2024-09-08 16:06:12 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//task1
+// fetch('https://northwind.vercel.app/api/orders?authuser=2')
+//   .then(response => response.json()) 
+//   .then(data => {
+    
+//     for (let i = 0; i < data.length; i++) {
+//       if (data[i].id === 11075) {  
+//         console.log(data[i]);      
+//         break;  
+//       }
+//     }
+//   })
+
+
+
+  
+
+// task2
+// fetch('https://northwind.vercel.app/api/orders?authuser=2')
+// .then(response => response.json())  
+// .then(data => {
+//   let count = 0;  
+  
+//   for (let i = 0; i < data.length; i++) {
+//     if (data[i].shipAddress.country === 'USA') {
+//       count++;  
+//     }
+//   }
+  
+//   console.log('say:', count);  
+// })
+
